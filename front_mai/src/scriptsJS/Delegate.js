4324044778f6ea056d9ba1bd94e792c4effca1b3
Nandy Bâ <nandyba@yahoo.fr>
2023-02-09 15:26:50 +0100

    Reduction du nombre de states

--- front_mai/src/scriptsJS/Delegate.js
+++ front_mai/src/scriptsJS/Delegate.js
@@ -15,31 +15,23 @@ function Delegate(){
     const WethAbi = require("../ABIS/WethVault.json").abi;
     const WethContract = new ethers.Contract(WethAddress, WethAbi, provider);
 
-    const [tokenId, setTokenId] = useState(0); 
-    const [vaultName, setName] = useState(""); 
-    const [owner, setOwnerAddress] = useState(""); 
-    const [borrower, setBorrowerAddress] = useState(""); 
-    const [amount, setAmount] = useState(0);
-    const [text, setText] = useState("Approve"); 
 
-    const handleChampToken = (event)=>{
-        setTokenId(event.target.value)
-    }
+    const [parameters, setParameters] = useState({
+                                                    tokenId:undefined,
+                                                    vaultName:undefined,
+                                                    owner:undefined,
+                                                    borrower:undefined,
+                                                    amount:undefined
+                                                });
+    const [text, setText] = useState("Approve");
 
     // a modifier plus tard par un menu déroulant pour sélectionner les différents vault présent sur le network
-    const handleChampVault = (event)=>{
-        setName(event.target.value)
-    }
-
-    const handleChampOwner = (event)=>{
-        setOwnerAddress(event.target.value)
-    } 
-    const handleChampBorrower = (event)=>{
-        setBorrowerAddress(event.target.value)
-    } 
-    const handleChampAmount = (event)=>{
-        setAmount(event.target.value)
-    }
+    const handleInput = (field) => (event) => {
+        setParameters({
+            ...parameters,
+            [field]: event.target.value
+        });
+    };
 
     const handleClick = async () => {
         
@@ -52,12 +44,12 @@ function Delegate(){
 
     async function approveERC721(){
 
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    const WethContract = new ethers.Contract(WethAddress, WethAbi, signer);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const WethContract = new ethers.Contract(WethAddress, WethAbi, signer);
 
-    const tx = await WethContract.approve(delegateAddress,tokenId ); 
-    console.log("Tx hash : ${tx.hash}"); 
+        const tx = await WethContract.approve(delegateAddress, parameters.tokenId); 
+        console.log("Tx hash : ${tx.hash}"); 
     }
 
      async function depositERC721(){
@@ -65,7 +57,7 @@ function Delegate(){
         await provider.send("eth_requestAccounts", []);
         const signer = provider.getSigner();
         const delegateContract = new ethers.Contract(delegateAddress, delegateAbi, provider, signer);
-        const tx = await delegateContract.depositERC721(tokenId, vaultName); 
+        const tx = await delegateContract.depositERC721(parameters.tokenId, parameters.vaultName); 
         console.log("Tx hash : ${tx.hash}"); 
 
     }
@@ -76,27 +68,27 @@ function Delegate(){
         const signer = provider.getSigner();
 
         const delegateContract = new ethers.Contract(delegateAddress, delegateAbi, provider, signer);
-        const tx = await delegateContract.approveDelegation(owner, borrower, vaultName, tokenId, amount);
+        const tx = await delegateContract.approveDelegation(parameters.owner, parameters.borrower, parameters.vaultName, parameters.tokenId, parameters.amount);
         console.log("Tx hash : ${tx.hash}"); 
 
     }
     return(
         <div>
             <text>tokenID : </text>
-            <input className="barre" type="number"value={tokenId} onChange={e=>handleChampToken(e)}/>
+            <input className="barre" type="number"value={parameters.tokenId} onChange={handleInput("tokenId")}/>
             
             <br></br>
             <text>Vault : </text>
-            <input className="barre" type="text"value={vaultName} onChange={e=>handleChampVault(e)}/>
+            <input className="barre" type="text"value={parameters.vaultName} onChange={handleInput("vaultName")}/>
             <br></br>
             <text>Lender address : </text>
-            <input className="barre" type="text"value={owner} onChange={e=>handleChampOwner(e)}/>
+            <input className="barre" type="text"value={parameters.owner} onChange={handleInput("owner")}/>
             <br></br>
             <text>Borrower address : </text>
-            <input className="barre" type="text"value={borrower} onChange={e=>handleChampBorrower(e)}/>
+            <input className="barre" type="text"value={parameters.borrower} onChange={handleInput("borrower")}/>
             <br></br>
             <text>Amount : </text>
-            <input className="barre" type="text"value={amount} onChange={e=>handleChampAmount(e)}/>
+            <input className="barre" type="text"value={parameters.amount} onChange={handleInput("amount")}/>
             <div>
             <button onClick={handleClick}>{text}</button>
             
